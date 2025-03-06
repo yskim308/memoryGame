fffbb1ec4df1a2b870e35b1b6e7f64e234797530
Young Seo Kim <youngseokim7@gmail.com>
2025-03-06 19:55:15 +0900

    initial implementation of app and basic logic

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
 import { CardObject } from "./types";
-import getGifs from "./utils";
+import getGifs, { shuffleCards } from "./utils";
+const MAX_CARDS = 20;
 
 export default function App() {
-  const [clickedCards, setClickedCards] = useState<number[]>([]);
+  const [clickedCards, setClickedCards] = useState<string[]>([]);
   const [gameCount, setGameCount] = useState<number>(0);
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
   const [cardArray, setCardArray] = useState<CardObject[]>([]);
 
   useEffect(() => {
-    getGifs().then((resolvedCardArray: CardObject[]) => {
+    getGifs("cats", `${MAX_CARDS}`).then((resolvedCardArray: CardObject[]) => {
       setCardArray(resolvedCardArray);
     });
   }, []);
+
+  const onCardClick = (card: CardObject) => {
+    const isClicked: boolean = clickedCards.includes(card.id) ? true : false;
+    if (isClicked) {
+      setClickedCards([]);
+      setGameCount(0);
+      setIsGameOver(true);
+    } else {
+      if (gameCount == MAX_CARDS - 1) {
+        // todo: the logic should be seperated from generic game over if the user actually wins
+        setClickedCards([]);
+        setGameCount(0);
+        setIsGameOver(true);
+      } else {
+        const shuffled: CardObject[] = shuffleCards(cardArray);
+        setCardArray(shuffled);
+        setClickedCards([...clickedCards, card.id]);
+        setGameCount(gameCount + 1);
+      }
+    }
+  };
+
+  return ()
 }
