f1cb0631e5334b16a614f20bdef82028e01075da
Young Seo Kim <youngseokim7@gmail.com>
2025-03-06 19:28:32 +0900

    set cardarray in main app

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CardObject } from "./types";
+import getGifs from "./utils";
 
 export default function App() {
-  const [clickedCards, setClickedCards] = useState<CardObject[]>([]);
+  const [clickedCards, setClickedCards] = useState<number[]>([]);
   const [gameCount, setGameCount] = useState<number>(0);
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
-  let cardArray: CardObject[];
+  const [cardArray, setCardArray] = useState<CardObject[]>([]);
+
+  useEffect(() => {
+    getGifs().then((resolvedCardArray: CardObject[]) => {
+      setCardArray(resolvedCardArray);
+    });
+  }, []);
 }
