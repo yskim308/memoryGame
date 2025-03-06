ce0758fae427e482578004166e8cf9f909eb30c0
Young Seo Kim <youngseokim7@gmail.com>
2025-03-06 20:15:09 +0900

    initial implementation of Card component

---
+++ src/Card.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { CardObject, CardProps } from "./types";
+
+export default function Card({ card, handleClick }: CardProps) {
+  const [hovered, setHovered] = useState<boolean>(false);
+  return (
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className={`font-bold ${hovered ? "bg-red-500" : ""}`}
+      onClick={() => handleClick(card)}
+    >
+      <img src={card.url} alt="fuk?" />
+      <h1>{card.desc}</h1>
+    </div>
+  );
+}
