6ebe066a6d4bd58ae8e04f849adaa6e7a72270db
Young Seo Kim <youngseokim7@gmail.com>
2025-03-06 19:14:03 +0900

    implement the getgifs async function

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,27 @@
-export default async function getGifs() {
-  const key = "AIzaSyAua1ztHOGQCQwOBkzJsaxe1zz71HURuEQ";
+import { CardObject, GiphyObject } from "./types";
+
+export default async function getGifs(): Promise<CardObject[]> {
+  const key = "srJ0aPiyv8XaGUvreaq3c50dSFOujB1C";
   const q = "cats";
   const limit = "20";
+  const cardArray: CardObject[] = [];
   try {
     const response = await fetch(
-      `https://g.tenor.com/v1/search?key=${key}&q=${q}&limit=${limit}`,
+      `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${q}&limit=${limit}`,
     );
-    const gifs = await response.json();
-    console.log(gifs);
+    const gifs: GiphyObject[] = await response.json();
+    gifs.map((gif) => {
+      cardArray.push({
+        url: gif.url,
+        id: gif.id,
+        desc: gif.title,
+      });
+    });
+    return cardArray;
   } catch (err) {
     console.log(err);
+    return [];
   } finally {
     console.log("all done");
   }
 }
-
-getGifs();
