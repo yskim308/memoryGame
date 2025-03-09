adc8cc38f70d1385b97a67a4db5d0c5d17db17fb
Young Seo Kim <youngseokim7@gmail.com>
2025-03-09 22:01:56 +0900

    basic styling for game ending divs

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -2,15 +2,15 @@ export { GameOver, GameWon };
 
 function GameOver() {
   return (
-    <div className="text-xl font-bold">
-      <p>Game over dumbass!</p>
+    <div className="text-xl font-bold w-full bg-red-300 text-center">
+      <p>Game Over</p>
     </div>
   );
 }
 
 function GameWon() {
   return (
-    <div className="text-xl font-bold">
+    <div className="text-xl font-bold w-full bg-green-300 text-center">
       <p>Game Won!</p>
     </div>
   );
