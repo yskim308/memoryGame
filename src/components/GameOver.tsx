dff301a19a7ef94dfaf1b7e1ab8e4e828effa01c
Young Seo Kim <youngseokim7@gmail.com>
2025-03-13 11:40:19 +0900

    add animation to gameover and gamewon alerts

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -2,7 +2,7 @@ export { GameOver, GameWon };
 
 function GameOver() {
   return (
-    <div className="fixed inset-0 flex justify-center items-center">
+    <div className="animate-fade-down animate-duration-300 fixed inset-0 flex justify-center items-center">
       <div className="flex bg-red-300 h-1/2 w-1/2  rounded-3xl justify-center items-center">
         <p className="font-sans font-bold text-4xl">Game Over</p>
       </div>
@@ -12,7 +12,7 @@ function GameOver() {
 
 function GameWon() {
   return (
-    <div className="fixed inset-0 flex justify-center items-center">
+    <div className="animate-fade-down animate-duration-300 fixed inset-0 flex justify-center items-center">
       <div className="flex bg-green-300 h-1/2 w-1/2  rounded-3xl justify-center items-center">
         <p className="font-sans font-bold text-4xl">Game Won</p>
       </div>
